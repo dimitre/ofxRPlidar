57d3fd454e0e6f14f434f87a1c833110d8e2a5ed
Dimitre <dimitre@users.noreply.github.com>
2025-02-08 16:12:15 -0300

    add Lidar project

---
+++ Lidar/src/ofApp.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ofMain.h"
+//#include "ofxRPlidar.h"
+#include "ofxMicroUI.h"
+//#include "ofxMicroUISoftware.h"
+
+
+class ofApp : public ofBaseApp{
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void keyPressed(int key);
+	void keyReleased(int key);
+	
+	ofxMicroUI u { "u.txt" };
+	
+	
+	
+private:
+//	std::vector<std::shared_ptr<ofxRPlidar>> sensors_;
+	
+	
+};
